87f333e72d69ae8f275fffdfa62901cb8ddd97cf
João Vitor Bezerra <jv23072006@gmail.com>
2025-02-19 15:04:10 +0000

    Remoção dos Antigos Diagramas - Casos de Uso e Casos de Uso por Módulo

--- LVJ_AdminVersion/obj/Debug/net8.0/LVJ_AdminVersion.AssemblyInfo.cs
+++ LVJ_AdminVersion/obj/Debug/net8.0/LVJ_AdminVersion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LVJ_AdminVersion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4512d4e0535fedc85b860afa4006a634b3143322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6698b9b939302fc36843b8c872eee1197016caea")]
 [assembly: System.Reflection.AssemblyProductAttribute("LVJ_AdminVersion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LVJ_AdminVersion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
